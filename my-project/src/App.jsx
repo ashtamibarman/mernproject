ff56537e4685cdee705ee2f31722902f84d68fb2
ashtami <ashtamibarman6@gmail.com>
2025-06-03 05:19:18 -0700

    Fix API URLs for deployment

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/login",
+        "http://your-backend.onrender.com/login",
         {
           username: form.username,
           password: form.password,
